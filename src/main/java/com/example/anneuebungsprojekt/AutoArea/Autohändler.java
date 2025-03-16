877368d609f15a41d300b6cd7d81c08b0fd8b39a
ThorstiFinki <105248638+ThorstiFinki@users.noreply.github.com>
2025-03-16 21:14:50 +0100

    Merge Konflikte behoben

--- src/main/java/com/example/anneuebungsprojekt/AutoArea/Autohändler.java
+++ src/main/java/com/example/anneuebungsprojekt/AutoArea/Autohändler.java
@@ -6,10 +6,8 @@ public class Autohändler {
         Auto auto1 = new Auto("Stadtwagen", 50);
         Auto auto2 = new Auto("Schneller Flitzer", 150);
         Auto auto3 = new Auto("Geländewagen", 250);
-        Taxi auto4 = new Taxi("Taxi klein", 101, 4, true);
+        Taxi auto4 = new Taxi("Taxi klein", 101, 3, true);
         Taxi auto5 = new Taxi("Taxi groß", 101, 8, false);
-        Taxi auto4 = new Taxi("Taxi klein", 101, 3);
-        Taxi auto5 = new Taxi("Taxi groß", 101, 8);
 
         auto1.setName("Super Duper Auto");
         auto4.setName("Rainers Auto");
