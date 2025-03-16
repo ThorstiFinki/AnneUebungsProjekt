b51620a529100d7e620e178de1f6580a395718be
ThorstiFinki <105248638+ThorstiFinki@users.noreply.github.com>
2025-03-16 20:56:11 +0100

    Taxilicht hinzugefügt

--- src/main/java/com/example/anneuebungsprojekt/AutoArea/Autohändler.java
+++ src/main/java/com/example/anneuebungsprojekt/AutoArea/Autohändler.java
@@ -6,8 +6,8 @@ public class Autohändler {
         Auto auto1 = new Auto("Stadtwagen", 50);
         Auto auto2 = new Auto("Schneller Flitzer", 150);
         Auto auto3 = new Auto("Geländewagen", 250);
-        Taxi auto4 = new Taxi("Taxi klein", 101, 4);
-        Taxi auto5 = new Taxi("Taxi groß", 101, 8);
+        Taxi auto4 = new Taxi("Taxi klein", 101, 4, true);
+        Taxi auto5 = new Taxi("Taxi groß", 101, 8, false);
 
         auto1.setName("Super Duper Auto");
         auto4.setName("Rainers Auto");
@@ -15,8 +15,10 @@ public class Autohändler {
         System.out.println("Beispiel 1 Name: " + auto1.getName() + " Typ: " + auto1.getTyp() + " PS Zahl: " + auto1.getPsZahl());
         System.out.println("Beispiel 2 Name: " + auto2.getName() + " Typ: " + auto2.getTyp() + " PS Zahl: " + auto2.getPsZahl() + " hat Klima ? " + auto2.isHatKlima());
         System.out.println("Beispiel 3 Name: " + auto3.getName() + " Typ: " + auto3.getTyp() + " PS Zahl: " + auto3.getPsZahl() + " hat Klima ? " + auto3.isHatKlima());
-        System.out.println("Beispiel 4 Name: " + auto4.getName() + " Typ: " + auto4.getTyp() + " PS Zahl: " + auto4.getPsZahl() + " hat Klima ? " + auto4.isHatKlima() + " Anzahl Passagiere: " + auto4.getAnzahlPassagiere());
-        System.out.println("Beispiel 5 Name: " + auto5.getName() + " Typ: " + auto5.getTyp() + " PS Zahl: " + auto5.getPsZahl() + " hat Klima ? " + auto5.isHatKlima() + " Anzahl Passagiere: " + auto5.getAnzahlPassagiere());
+        System.out.println("Beispiel 4 Name: " + auto4.getName() + " Typ: " + auto4.getTyp() + " PS Zahl: "
+                + auto4.getPsZahl() + " hat Klima ? " + auto4.isHatKlima() + " Anzahl Passagiere: " + auto4.getAnzahlPassagiere()+ "Taxilicht an? " + auto4.isTaxiLicht()) ;
+        System.out.println("Beispiel 5 Name: " + auto5.getName() + " Typ: " + auto5.getTyp() + " PS Zahl: "
+                + auto5.getPsZahl() + " hat Klima ? " + auto5.isHatKlima() + " Anzahl Passagiere: " + auto5.getAnzahlPassagiere()+ "Taxilicht an? " + auto5.isTaxiLicht());
 
     }
 }
--- src/main/java/com/example/anneuebungsprojekt/AutoArea/Taxi.java
+++ src/main/java/com/example/anneuebungsprojekt/AutoArea/Taxi.java
@@ -2,15 +2,22 @@ package com.example.anneuebungsprojekt.AutoArea;
 
 public class Taxi extends Auto {
 
-    public int getAnzahlPassagiere() {
-        return anzahlPassagiere;
-    }
+
 
     private int anzahlPassagiere;
 
-    public Taxi(String typ, int pS, int anzahlPassagiere) {
+    private boolean taxiLicht;
+
+    public Taxi(String typ, int pS, int anzahlPassagiere, boolean taxiLicht) {
         super(typ, pS);
         this.anzahlPassagiere = anzahlPassagiere;
+        this.taxiLicht = taxiLicht;
     }
 
+    public int getAnzahlPassagiere() {
+        return anzahlPassagiere;
+    }
+    public boolean isTaxiLicht() {
+        return taxiLicht;
+    }
 }
